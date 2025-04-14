02c0490b4a2535f3bd4ec6541cbb4c6e68800d97
HalleTim <halletim@web.de>
2025-04-14 21:33:22 +0200

    add braitenberg

---
+++ braitenbergChallenge/packages/solution/connections.py
@@ -0,0 +1,22 @@
+from typing import Tuple
+
+import numpy as np
+
+
+def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
+    # TODO: write your function instead of this one
+    res = np.zeros(shape=shape, dtype="float32")
+    # these are random values
+    res[300:, 150:320] = 1
+    #res[350:, 320:500] = -1
+    # ---
+    return res
+
+
+def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
+    # TODO: write your function instead of this one
+    res = np.zeros(shape=shape, dtype="float32")
+    # these are random values
+    res[300:, 320:600] = 1
+    # ---
+    return res
---
+++ braitenbergChallenge/packages/solution/preprocessing.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+lower_hsv = np.array([15, 45, 69])
+upper_hsv = np.array([29, 255, 255])
+
+
+def preprocess(image_rgb: np.ndarray) -> np.ndarray:
+    """Returns a 2D array"""
+    hsv = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2HSV)
+    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+    return mask
